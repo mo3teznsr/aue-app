dfc2e6723dc69548c41b66b750651593ecc76268
Mohammad Naved <kmohdnaved@gmail.com>
2024-01-13 13:17:39 +0400

    Home page dont have account text remove

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,16 +24,14 @@ const Home: React.FC = () => {
 
       <h1 style={{ fontSize: '60px', color: '#3880ff', textAlign: 'center', marginTop:'50px' }}>السنع الامارات</h1>
 
-        <div style={{display:"flex",alignItems:"center",width:"100%",marginTop:"4rem",marginBottom:"2rem",justifyContent:"center"}}>
+        <div style={{display:"flex",alignItems:"center",width:"100%",marginTop:"4rem",marginBottom:"4rem",justifyContent:"center"}}>
 
         <img src="logo.png" style={{height:120}} />
         </div>
 
         <div>
      
-        <div>
-          <span>ليس لديك حساب؟</span> <Button onClick={()=>history.push('register')}>تسجيل</Button>
-        </div>
+        
 
         <Button variant="contained" onClick={go} color="secondary" style={{background:"#fff",marginTop:".5rem"}}  fullWidth >
          <img src="uae-pass.png" style={{height:20,marginInlineEnd:".5rem"}}  /> <span style={{color:"#000"}}>الدخول بالهوية الاماراتية</span>
