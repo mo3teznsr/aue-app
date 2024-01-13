27fbfbada37979dc801daabfcd114cc85eb1fb6e
Mohammad Naved <kmohdnaved@gmail.com>
2024-01-13 12:45:28 +0400

    gmail-login and password input remove

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,62 +18,19 @@ const Home: React.FC = () => {
   const go=()=>{history.push("/tabs")}
   return (
     <IonPage>
+      
    
       <IonContent className="ion-padding"  >
-        <div style={{display:"flex",alignItems:"center",width:"100%",marginTop:"5rem",marginBottom:"2rem",justifyContent:"center"}}>
+
+      <h1 style={{ fontSize: '60px', color: '#3880ff', textAlign: 'center', marginTop:'50px' }}>السنع الامارات</h1>
+
+        <div style={{display:"flex",alignItems:"center",width:"100%",marginTop:"4rem",marginBottom:"2rem",justifyContent:"center"}}>
 
         <img src="logo.png" style={{height:120}} />
         </div>
 
         <div>
-     <IonLabel>البريد الالكتروني</IonLabel>
-        <TextField variant="outlined" 
-       
-        fullWidth margin="dense"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Email />
-            </InputAdornment>
-          ),
-        }} />
-<IonLabel> كلمة المرور</IonLabel>
-<TextField variant="outlined" 
-       
-        margin="dense"
-        fullWidth 
-        type={showPassword?"text":"password"}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Lock />
-            </InputAdornment>
-          ),
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton onClick={()=>setShowPassword(!showPassword)}>
-                {
-                  showPassword?<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                  <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-                </svg>:<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye-closed" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M21 9c-2.4 2.667 -5.4 4 -9 4c-3.6 0 -6.6 -1.333 -9 -4" />
-                  <path d="M3 15l2.5 -3.8" />
-                  <path d="M21 14.976l-2.492 -3.776" />
-                  <path d="M9 17l.5 -4" />
-                  <path d="M15 17l-.5 -4" />
-                </svg>
-                }
-              </IconButton>
-              </InputAdornment>
-              )
-        }} />
-
-        <Button variant="contained"  onClick={go}  fullWidth >
-          تسجيل الدخول
-        </Button>
+     
         <div>
           <span>ليس لديك حساب؟</span> <Button onClick={()=>history.push('register')}>تسجيل</Button>
         </div>
